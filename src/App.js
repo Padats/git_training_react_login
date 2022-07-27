84ca38a17a4da0d1d923a6793673d7fb38576a8f
Padate Modiaba <pmodiaba.rtei@gmail.com>
2022-07-27 12:29:14 +0400

    changed to fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {Fragment, useContext} from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -8,13 +8,13 @@ import AuthContext from "./store/auth-context";
 function App() {
   const ctx = useContext(AuthContext);
   return (
-    <React.Fragment>
+    <Fragment>
       <MainHeader />
       <main>
         {!ctx.isLoggedIn && <Login />}
         {ctx.isLoggedIn && <Home />}
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
